8841b81c386101227f396397f92b436c376b8b45
Jhon Antony <jhonatoni9.81@gmail.com>
2024-05-16 12:55:25 -0500

    Agregamos la seccion de about skills projects contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,91 +1,40 @@
-
-  // getElementById - busca un elemento por su id
-  const menu = document.getElementById('menu')
-  const nav = document.querySelector('.nav')
-  // const btnOpen = document.getElementById('btnOpen')
-  // const btnClose = document.getElementById('btnClose')
-
-  // function openMenu () {
-  //   menu.classList.add('show-menu')
-  // }
-
-  // function closeMenu () {
-  //   menu.classList.remove('show-menu')
-  // }
-
-// const pokemon = document.getElementById('pokemon');
-
-
-// pokemon.onclick = function(){
-//     alert('Atrapaste un pokemon');
-//   }
-
-  // esta bien
-  // btnOpen.onclick = openMenu
-  // btnClose.onclick = closeMenu
-
-  //const box = document.querySelector('.box');
-
-  //box.className = 'caja';
-
-  // querySelector 
-
-//  const h1 = document.querySelector('h1');
-
-//  h1.innerText = 'hola mundo';
-
-
-
-//hay tres formas de ejecutar un handler de eventos
-// 1. atributo de evento en HTML >> osea directamente en el html onclick
-// 2. propiedades de eventos
-
-// se recomeintda el querySelector[All] para seleccionar elementos
-
-//  const btnPokemon = document.querySelector()
-
-// 3. addEventListener  
-
-
-// btnOpen.addEventListener('click',openMenu)
-// btnClose.addEventListener('click',closeMenu)
-
-// delegar eventos con if
+/* ====================== NAVBAR ====================== */
+const menu = document.getElementById('menu')
+const nav = document.querySelector('.nav')
 const links = nav.querySelectorAll('.menu__link')
 
-nav.addEventListener('click',function (event){ // handler es una funcion anonima
 
- if(event.target.closest('#btnOpen')){
-  menu.classList.add('show-menu')
- }
-
- if(event.target.closest('#btnClose')){
-  menu.classList.remove('show-menu')
- }
-
-
- if (event.target.matches('.menu__link')){
-  menu.classList.remove('show-menu')
-
-  for (const link of links){
-    link.classList.remove('active')
+nav.addEventListener('click', function (event) {
+  if (event.target.closest('#btnOpen')) {
+    menu.classList.add('show-menu')
   }
 
-  event.target.classList.add('active')
- }
- 
+  if (event.target.closest('#btnClose')) {
+    menu.classList.remove('show-menu')
+  }
+
+  if (event.target.matches('.menu__link')) {
+    menu.classList.remove('show-menu')
+
+    for (const link of links) {
+      link.classList.remove('active')
+    }
+
+    event.target.classList.add('active')
+  }
 })
 
+/* =================== Dark Mode =================== */
 const body = document.body
 const ls = window.localStorage
 const btnTheme = document.getElementById('btnTheme')
 
-
-const theme = ls.getItem('darkMode');
+// Preguntamos si esta activado el darkMode en el localStorage
+const theme = ls.getItem('darkMode')
 const sun = 'bx bxs-sun'
 const moon = 'bx bxs-moon'
 
-if(theme) {
+if (theme) {
   body.classList.add('dark')
   btnTheme.firstElementChild.className = sun
 } else {
@@ -93,30 +42,42 @@ if(theme) {
   btnTheme.firstElementChild.className = moon
 }
 
-btnTheme.addEventListener('click',function(){
-
-  body.classList.toggle('dark');
-
+btnTheme.addEventListener('click', function () {
+  body.classList.toggle('dark')
   if (body.classList.contains('dark')) {
+    // entonces guardo en el localStorage el valor de dark
     ls.setItem('darkMode', true)
     btnTheme.firstElementChild.className = sun
   } else {
+    // entonces voy a eliminar del localStorage el valor de dark
     ls.removeItem('darkMode')
     btnTheme.firstElementChild.className = moon
   }
 
-
-
 })
 
-//   typing
-
+/* =================== Typing =================== */
 var typed = new Typed('#typing', {
-  strings: ['<i>Frontend </i> Developer', 'Web Developer', 'Fullstack Developer'],
+  strings: ['Frontend Developer', 'Web Developer', 'Designer', 'Fullstack Developer'],
   typeSpeed: 50,
   backSpeed: 50,
   startDelay: 25,
   loop: true,
   loopCount: Infinity,
-});
+  cursorChar: '_',
+})
 
+/* =================== ScrollReveal =================== */
+
+const sr = new ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+sr.reveal('.hero__content-img', { origin: 'right', distance: '100px' })
+sr.reveal('.hero__content-body', { origin: 'left', distance: '100px' })
+sr.reveal('.about p', { origin: 'bottom', distance: '100px', duration: 2000 })
+sr.reveal('.about p+p', { origin: 'bottom', distance: '100px', duration: 3000 })
+sr.reveal('.skills__content', { origin: 'top', distance: '100px' })
